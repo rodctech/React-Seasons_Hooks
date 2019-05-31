b40ce308404fba90d46c057807e53becd3c0dce7
Rodrigo Chavez <rodctech@gmail.com>
2019-05-31 10:37:56 -0500

    334. Basic Seasons App before Hooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);  // WE need the super to pass the React Components
+
+        //If need to do direct assignment, this is the only place/time
+        //   this.state = { lat: null, errorMessage: ''};
+
+    }
+
+    state = {lat: null, errorMessage: ''};  //Diferent way to set state instead of constructor
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({lat: position.coords.latitude}),
+            // We Called setSate!
+            // never use assigment ex."this.state.lat = pos.coor.lat"
+            (err) =>
+                this.setState({errorMessage: err.message})
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was updated - it rerendered!')
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+
+        return <Spinner message={"Please accept location request"}/>;
+    }
+
+    // WE HAVE TO DEFINE RENDER!!
+    render() {
+        return (
+            <div className={"border red"}>
+                {this.renderContent()}
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
