b40ab9da2e77d12e0caed00c4e6449812d95c0ff
Rodrigo Chavez <rodctech@gmail.com>
2019-05-31 10:50:46 -0500

    334. Refactored App for USE of HOOKS Working now as Functional Component using hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from './Spinner';
 
+const App = () => {
+    const [lat, setLat] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => setLat(position.coords.latitude),
+            (err) => setErrorMessage(err.message)
+        );
+    }, [])
+
+    let content;
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat}/>
+    } else {
+        content = <Spinner message={"Please accept location request"}/>;
+    }
+
+    return <div className={"border red"}>{content}</div>
+};
+
+/*  REFACTORED Into Functional Component FOR USE OF HOOKS.
 class App extends React.Component {
 
     constructor(props) {
@@ -52,6 +74,7 @@ class App extends React.Component {
         );
     }
 }
+*/
 
 
 ReactDOM.render(
